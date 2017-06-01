c94bcab7a7af4abd5f7f43ef52be249f010b2296
safouene <benalaya.safouene.com>
2017-06-01 22:46:08 +0100

    setup git push with heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
-
+const port = process.env.PORT || 3000; 
 var app = express();
 
 app.set('view engine', 'hbs');
@@ -32,6 +32,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(Server is up on port = ${port}`);
 });
