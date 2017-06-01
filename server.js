cb8bfad7543b9cba52d360f4fd87a18c5e97fe68
safouene <benalaya.safouene.com>
2017-06-01 22:57:40 +0100

    bug fixes

--- server.js
+++ server.js
@@ -6,14 +6,12 @@ var app = express();
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
-app.use((req,res,next) => {
-  res.render('maintenance.hbs');
-});
+
 
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
-    welcomeMessage: 'Welcome to my website',
+    welcomeMessage: 'Welcome to my website el 7AAAAAAA',
     currentYear: new Date().getFullYear()
   });
 });
